132e3dd46431b2eb1a9f4e78331f372ee0a7f1be
Thitirat99 <6406021631043@fitm.kmutnb.ac.th>
2022-12-30 20:21:59 +0700

    up

---
+++ Lecture-03 ArrayLoop/maparray.js
@@ -0,0 +1,5 @@
+const finalParticipants = ["Taytor", "Donald","Don","Natash","Bobby"];
+
+const announcements = finalParticipants.map((member) =>{return member + "joined the contest.";});
+
+console.log(announcements);
