cad22c1fc9682c261a8cbac63199d8ebeb376922
Thitirat99 <6406021631043@fitm.kmutnb.ac.th>
2023-01-04 23:40:05 +0700

    up

--- Lecture-03 ArrayLoop/Ex02.js
+++ Lecture-03 ArrayLoop/Ex02.js
@@ -1,6 +1,15 @@
-for (let char of "Table" ){
-    console.log (char);
+let furniture = 'Table';
+let furniture1 = 'chairs';
+let furniture2 = 'Couch';
+    for (let );
+    {
+    console.log(furniture);
+    console.log(furniture1);
+    console.log(furniture2);
+   
 
-}
+    }
+
+
 
 
