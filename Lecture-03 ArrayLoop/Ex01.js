a470b5d4a4758acb3700be44c73b4d8f54851f7a
Thitirat99 <6406021631043@fitm.kmutnb.ac.th>
2023-01-05 22:57:33 +0700

    up

--- Lecture-03 ArrayLoop/Ex01.js
+++ Lecture-03 ArrayLoop/Ex01.js
@@ -1,3 +1,11 @@
-let values1= ['Apple',1,false];
-let values2= ['fries',2,true];
-let values3= ['Mars',9,'Apple'];
+let values1 = ['Apple',1,false];
+let values2 = ['fries',2,true];
+let values3 = ['Mars',9,'Apple'];
+
+let sumValue = values1.concat(values2.concat(values3))
+
+for(let i = 0; i < sumValue.length; i++){
+    for(let j = i+1;j < sumValue.length;j++)
+    if(sumValue[i] ===  sumValue[j])
+    console.log(sumValue[i] + "=" + sumValue[j]);
+}
